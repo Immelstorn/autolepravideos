22be50307b245bb525eb120eaa014aed55e289b4
Denis Volovenko <dvo@dandomain.dk>
2017-03-20 11:32:23 +0200

    change url

--- AutoLepraTop.Web/Scripts/app/api.js
+++ AutoLepraTop.Web/Scripts/app/api.js
@@ -1,7 +1,7 @@
 (function() {
 
     var api = function($http) {
-        var baseUrl = "http://lepraTop.api/api/auto";
+        var baseUrl = "http://autolepratop.azurewebsites.net/api/auto";
 
         var getList = function(page, sort) {
             var url = baseUrl + "?page=" + (page == null ? 1 : page) + "&sort=" + (sort == null ? "byrating" : sort);
@@ -11,32 +11,8 @@
                 });
         };
 
-//        var getDetails = function(appId) {
-//            return $http.get(baseUrl + "/" + appId)
-//                .then(function(response) {
-//                    return response;
-//                });
-//        };
-//
-//        var getManager = function() {
-//            return $http.get(baseUrl + "/manager")
-//                .then(function(response) {
-//                    return response;
-//                });
-//        };
-//
-//        var loadMoreApps = function(amountForLoading) {
-//            return $http.post(baseUrl + "/loadMore?amount=" + amountForLoading)
-//                .then(function (response) {
-//                    return response;
-//                });
-//        };
-
         return {
-            getList: getList,
-//            getDetails: getDetails,
-//            getManager: getManager,
-//            loadMoreApps: loadMoreApps
+            getList: getList
         };
 
     };
