93c3a7278e80100f907400f97b6034c3c73de75a
Denis Volovenko <dvo@dandomain.dk>
2017-03-20 15:20:07 +0200

    change url

--- AutoLepraTop.Web/Scripts/app/api.js
+++ AutoLepraTop.Web/Scripts/app/api.js
@@ -1,7 +1,7 @@
 (function() {
 
     var api = function($http) {
-        var baseUrl = "http://autolepratop.azurewebsites.net/api/auto";
+        var baseUrl = "http://autolepratopapi.azurewebsites.net/api/auto";
 
         var getList = function(page, sort) {
             var url = baseUrl + "?page=" + (page == null ? 1 : page) + "&sort=" + (sort == null ? "byrating" : sort);
